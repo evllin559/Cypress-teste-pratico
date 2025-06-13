926ee7ee98501fd5487a244761d65592a75a453d
evlin tavares de souza <evlinsouza362@gmail.com>
2025-06-13 17:40:37 -0300

    Update spec.cy.js

--- spec.cy.js
+++ spec.cy.js
@@ -1,3 +1,20 @@
+// support/e2e.js ou support/commands.js
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
+// login.cy.js ou onde estiver teu teste
+describe('Login Kabum Evlin', () => {
+  it('Deve logar com email e senha corretos', () => {
+    cy.visit('https://www.kabum.com.br/login')
+
+    cy.get('[data-testid="login-input"]').type('evlinsouza362@gmail.com')
+
+    cy.get('input[name="password"]').type('Evlin@24')
+
+    cy.get('button[type="submit"]').click()
+  })
+})
 
 
 it('Deve adicionar ao carrinho (true) e depois remover (false)', () => {
@@ -5,7 +22,6 @@ it('Deve adicionar ao carrinho (true) e depois remover (false)', () => {
 
   
 cy.get('#purchaseButtonMobile').click();
-cy.visit('https://www.kabum.com.br/carrinho').click();
+// cy.visit('https://www.kabum.com.br/carrinho').click();
 
 });
-
